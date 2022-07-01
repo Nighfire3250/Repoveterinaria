1af7973c972d7af6a4ce5e04b1af3615f86d30dd
unknown <Alvarohaza2@gmail.com>
2022-06-30 20:54:09 -0600

    derrotadaaaa

--- src/views/AdminPage/Sections/Facturas/listFacturas.jsx
+++ src/views/AdminPage/Sections/Facturas/listFacturas.jsx
@@ -4,6 +4,7 @@
 import React from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import FileOpenIcon from "@mui/icons-material/FileOpen";
+import Pdf from "assets/img/articulos/Factura.pdf";
 import "./listFacturas.scss";
 
 const columns = [
@@ -16,14 +17,14 @@ const columns = [
       width: 160,
       renderCell: (params) => {
         return(
-            <button className="facturaButton"><FileOpenIcon /></button>
+            <a className="facturaButton" href={Pdf} target="_blank" rel="noreferrer"><FileOpenIcon /></a>
         );
       },
     },
 ];
 
 const rows = [
-    { id: 1, cliente: "Ernesto", fecha: "1/7/2022", total: 45 },
+    { id: 1, cliente: "Ernesto", fecha: "1/7/2022", total: 852 },
 ];
 
 
