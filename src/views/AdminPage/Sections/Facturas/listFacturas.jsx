1f790e6169dd638afa8b3f3e77af0ca10e351a06
Ernesto <ernestoraf2011@gmail.com>
2022-06-30 19:58:34 -0600

    modart

--- src/views/AdminPage/Sections/Facturas/listFacturas.jsx
+++ src/views/AdminPage/Sections/Facturas/listFacturas.jsx
@@ -1,9 +1,43 @@
+/* eslint-disable react/display-name */
+/* eslint-disable no-unused-vars */
 /* eslint-disable prettier/prettier */
 import React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import FileOpenIcon from "@mui/icons-material/FileOpen";
 import "./listFacturas.scss";
+
+const columns = [
+    { field: "id", headerName: "ID", width: 70 },
+    { field: "cliente", headerName: "Cliente", width: 160 },
+    { field: "fecha", headerName: "Fecha", width: 160 },
+    { field: "total", headerName: "Total C$", type: "number", width: 160 },
+    { field: "abrir",
+      headerName: "Abrir",
+      width: 160,
+      renderCell: (params) => {
+        return(
+            <button className="facturaButton"><FileOpenIcon /></button>
+        );
+      },
+    },
+];
+
+const rows = [
+    { id: 1, cliente: "Ernesto", fecha: "1/7/2022", total: 45 },
+];
+
 
 export default function ListFacturas() {
     return(
-        <div className="listFactura">Factura</div>
+        <div className="listFactura">
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                disableSelectionOnClick
+                pageSize={9}
+                rowsPerPageOptions={[9]}
+                checkboxSelection
+            />
+        </div>
     );
 }
